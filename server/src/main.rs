5ea1644f714ce1fbdfca8daa80cb1ae2e35896cd
borisnezlobin <me@borisnezlobin.com>
2024-03-07 13:48:26 -0800

    styles, pretty colors, and auto-append .html for pages

--- server/src/main.rs
+++ server/src/main.rs
@@ -4,6 +4,8 @@ use std::{
     net::{TcpListener, TcpStream},
 };
 
+use colored::Colorize;
+
 mod server;
 
 struct ResponseStructure<'a> {
@@ -13,7 +15,9 @@ struct ResponseStructure<'a> {
 }
 
 fn main() {
-    let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
+    let addr = "127.0.0.1:7878";
+    let listener = TcpListener::bind(addr).unwrap();
+    println!("{}", format!("Listening on http://{addr}").bold().green());
 
     for stream in listener.incoming() {
         let stream = stream.unwrap();
@@ -38,7 +42,28 @@ fn handle_connection(mut stream: TcpStream) {
         data: fs::read("pages/404.html").unwrap(),
     };
     if let Some(path) = split.get(1) {
-        let contents = fs::read(format!("pages/{path}"));
+        let mut fpath = path.clone();
+        // check if path has a full stop
+        let mut hasDot = false;
+        for c in path.chars() {
+            if c.eq(&'.') {
+                hasDot = true;
+                break;
+            }
+        }
+
+        if (!hasDot) {
+            fpath = fpath + ".html";
+            println!(
+                "{}",
+                format!(
+                    "{}",
+                    "path does not contain full stop, appending .html".bright_blue()
+                )
+            );
+        }
+
+        let contents = fs::read(format!("pages/{fpath}"));
         if let Ok(content) = contents {
             res = ResponseStructure {
                 status: 200,
@@ -54,5 +79,25 @@ fn handle_connection(mut stream: TcpStream) {
     response.extend(res.data);
     stream.write_all(response.as_slice());
 
-    println!("{}: responded with {}", target, res.status);
+    // println!("{}: responded with {}", target, res.status);
+
+    log_response(target, res.status, res.status_message);
+}
+
+fn log_response(request: &String, res_status: i32, res_message: &str) {
+    if res_status == 200 {
+        println!(
+            "{} {}:   {}",
+            format!("{}", res_status).bold().green(),
+            res_message.green(),
+            request.blue()
+        );
+    } else {
+        println!(
+            "{} {}:  {}",
+            format!("{}", res_status).bold().red(),
+            res_message.red(),
+            format!("{} (FAILED)", request).red(),
+        );
+    }
 }
