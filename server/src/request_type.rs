7b79558640bbcc2abb867695dc2a685247d9cc1c
borisnezlobin <me@borisnezlobin.com>
2024-07-18 12:33:56 -0700

    tests

--- server/src/request_type.rs
+++ server/src/request_type.rs
@@ -1,3 +1,4 @@
+#[derive(Debug, PartialEq)]
 pub enum RequestType {
     GET,
     POST,
@@ -28,20 +29,49 @@ impl RequestType {
     }
 }
 
-impl PartialEq for RequestType {
-    fn eq(&self, other: &Self) -> bool {
-        match (self, other) {
-            (RequestType::GET, RequestType::GET)
-            | (RequestType::POST, RequestType::POST)
-            | (RequestType::HEAD, RequestType::HEAD)
-            | (RequestType::PUT, RequestType::PUT)
-            | (RequestType::DELETE, RequestType::DELETE)
-            | (RequestType::CONNECT, RequestType::CONNECT)
-            | (RequestType::OPTIONS, RequestType::OPTIONS)
-            | (RequestType::TRACE, RequestType::TRACE)
-            | (RequestType::PATCH, RequestType::PATCH)
-            | (RequestType::ALL, RequestType::ALL) => true,
-            _ => false,
-        }
+#[cfg(test)]
+mod request_type_tests {
+    #[test]
+    fn test_request_type_from_str() {
+        use super::RequestType;
+        assert_eq!(RequestType::from_str("GET").unwrap(), RequestType::GET);
+        assert_eq!(RequestType::from_str("POST").unwrap(), RequestType::POST);
+        assert_eq!(RequestType::from_str("HEAD").unwrap(), RequestType::HEAD);
+        assert_eq!(RequestType::from_str("PUT").unwrap(), RequestType::PUT);
+        assert_eq!(RequestType::from_str("DELETE").unwrap(), RequestType::DELETE);
+        assert_eq!(RequestType::from_str("CONNECT").unwrap(), RequestType::CONNECT);
+        assert_eq!(RequestType::from_str("TRACE").unwrap(), RequestType::TRACE);
+        assert_eq!(RequestType::from_str("OPTIONS").unwrap(), RequestType::OPTIONS);
+        assert_eq!(RequestType::from_str("PATCH").unwrap(), RequestType::PATCH);
+        assert_eq!(RequestType::from_str("INVALID").is_err(), true);
+    }
+
+    #[test]
+    fn test_eq() {
+        use super::RequestType;
+        assert_eq!(RequestType::GET, RequestType::GET);
+        assert_eq!(RequestType::POST, RequestType::POST);
+        assert_eq!(RequestType::HEAD, RequestType::HEAD);
+        assert_eq!(RequestType::PUT, RequestType::PUT);
+        assert_eq!(RequestType::DELETE, RequestType::DELETE);
+        assert_eq!(RequestType::CONNECT, RequestType::CONNECT);
+        assert_eq!(RequestType::TRACE, RequestType::TRACE);
+        assert_eq!(RequestType::OPTIONS, RequestType::OPTIONS);
+        assert_eq!(RequestType::PATCH, RequestType::PATCH);
+        assert_eq!(RequestType::ALL, RequestType::ALL);
+    }
+
+    #[test]
+    fn test_neq() {
+        use super::RequestType;
+        assert_ne!(RequestType::GET, RequestType::POST);
+        assert_ne!(RequestType::GET, RequestType::HEAD);
+        assert_ne!(RequestType::GET, RequestType::PUT);
+        assert_ne!(RequestType::GET, RequestType::DELETE);
+        assert_ne!(RequestType::GET, RequestType::CONNECT);
+        assert_ne!(RequestType::GET, RequestType::TRACE);
+        assert_ne!(RequestType::GET, RequestType::OPTIONS);
+        assert_ne!(RequestType::GET, RequestType::PATCH);
+        assert_ne!(RequestType::GET, RequestType::ALL);
     }
 }
